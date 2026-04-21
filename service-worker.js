8095fb624b9fb9f4d6e7cfdc4ed6a4a1410a1517
Henock2104 <henockkalambay869@gmail.com>
2026-04-21 09:39:43 +0100

    Application de gestion de tâches

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = "frameflow-v2";
+
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(CACHE).then(c=>c.addAll([
+"/",
+"/index.html",
+"/manifest.json"
+]))
+);
+});
+
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
+});
